9553bdcc9e767c76d043b2d6b4295650538274ec
raize <raizenkimochi12@gmail.com>
2020-02-09 14:21:22 +0700

    TUGAS 2

--- app/src/main/java/com/example/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Pratikum Pemrograman Mobile");
+        System.out.println("Nama    : AL BANNA ISMOYO AJI");
     }
 }
